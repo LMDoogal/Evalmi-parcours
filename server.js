013dc03818dbcf4fe79bad5b98660a87d48052d1
LMDoogal <62205829+LMDoogal@users.noreply.github.com>
2020-08-31 17:57:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -49,8 +49,8 @@ app.get('/', function (req, res) {
 });
 
 // START server
-app.listen(3054, function () {
-  console.log('server listening on port 3054!')
+app.listen(3000, function () {
+  console.log('server listening on port 3000!')
 });
 
 
